52e03945a98452c821cb24e5e732ea7dac112de2
owenrb <owenrb@localhost>
2021-06-06 01:05:05 +0800

    activate spring-boot

---
+++ src/main/java/io/owenrbee/team/MainApplication.java
@@ -0,0 +1,15 @@
+package io.owenrbee.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String ... argv) {
+		
+		SpringApplication.run(MainApplication.class, argv);
+		
+	}
+}
